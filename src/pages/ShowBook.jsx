0a8df8f5d3fdda8fdaeef0a945ac5999f2805d10
Utkarsh <utkarshjha.4009@gmail.com>
2024-01-08 17:00:32 +0530

    minor bug

--- src/pages/ShowBook.jsx
+++ src/pages/ShowBook.jsx
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(()=>{
     setLoading(true);
     axios
-    .get(`http://localhost:5555/books/${id}`)
+    .get(`https://booksapi-production-de10.up.railway.app/books/${id}`)
     .then((response) => {
         setBook(response.data);
         setLoading(false); 
